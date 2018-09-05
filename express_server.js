c7703d088bcef7b76599f457494317bfd5dd0672
Alfred Law <alfred.lawsn@gmail.com>
2018-09-05 19:00:37 +0000

    finished storing and deleting cookie

--- express_server.js
+++ express_server.js
@@ -1,11 +1,21 @@
 var express = require("express");
 var app = express();
+var cookieParser = require("cookie-parser");
 var PORT = 8080; // default port 8080
 
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(bodyParser.urlencoded({extended: true}));   // false => object returning will have only strings or arrays in the key-value pair
+                                                    // true => can have other values e.g. dates, undefined, null
+app.use(cookieParser());
 app.set("view engine", "ejs");
+
+//------------------------------------------------------------//
+
+
+
+
+
+//------------------------------------------------------------//
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -21,7 +31,7 @@ function generateRandomString() {
   return randomURL;
 }
 
-
+//------------------------------------------------------------//
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -31,17 +41,22 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+
+
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = { urls: urlDatabase, username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/u/:shortURL", (req, res) => {
@@ -49,7 +64,26 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
+//the connection between the .ejs pages and this server page
+//the shortURL is mapped to req.params.id, longURL is mapped to urlDatabase[req.params.id]
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { username: req.cookies["username"], shortURL: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+});
+
 //------------------------------------------------------------//
+
+//Cookie store
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.username);
+  res.redirect(`/urls`);
+});
+
+//Cookie delete
+app.post("/logout", (req, res) => {
+  res.clearCookie("username")
+  res.redirect(`/urls`);
+});
 
 //this takes the new URL from url/new, and adds it the the urlDatabase
 app.post("/urls", (req, res) => {
@@ -74,17 +108,16 @@ app.post("/urls/:id/delete", (req, res) => {
 app.post("/urls/:id/update", (req, res) => {
   console.log(req.body);  // debug statement to see POST parameters
   urlDatabase[req.params.id] = req.body.longURL;
-  res.redirect(`/urls/${req.params.id}`);
+  res.redirect(`/urls/`);
 });
+
+
+
+
 
 //------------------------------------------------------------//
 
-//the connection between the .ejs pages and this server page
-//the shortURL is mapped to req.params.id, longURL is mapped to urlDatabase[req.params.id]
-app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id] };
-  res.render("urls_show", templateVars);
-});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
